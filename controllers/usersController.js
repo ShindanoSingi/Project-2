36a81fea863eb7ade707abc21aa54766a71b0a12
shindano <shindanosingi1@gmail.com>
2022-04-26 02:08:55 -0500

    My project 2 with responsive (homepage, signUp anf singIn)

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,15 +4,6 @@ const axios = require('axios')
 const User = require("../models/users-models");
 
 let userTitle = '';
-let songs = [];
-
-// // I am using this youtube API. This API return results based on specified country
-// urlCountry = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1000000000&regionCode=TZ&key=AIzaSyA9ZcRubHdOkYqfjF3MVPhCLsp_fMgt1Ug"
-
-// This API searches by title
-url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1000000000&q=Rayvanny&key=AIzaSyA9ZcRubHdOkYqfjF3MVPhCLsp_fMgt1Ug"
-
-
 
 // Define a router
 const router = express.Router();
@@ -23,8 +14,18 @@ router.get("/", (req, res) => {
         .then((userData) => res.render('adminPage', { userData }));
 });
 
+// Add a user to the database, then go to the home screen
+router.post("/signUp", (req, res) => {
+    User.create(req.body)
+        .then(() => res.redirect('/homePage'));
+});
+
 router.get('/signUp', (req, res) => {
     res.render('signUpPage')
+})
+
+router.get('/homePage', (req, res) => {
+    res.render('homePage')
 })
 
 // When "Already signed in?" link is clicked, redirect to "sign in" page 
@@ -32,42 +33,53 @@ router.get('/signIn', (req, res) => {
     res.render('signInPage');
 })
 
-router.get('/homePage', (req, res) => {
-    res.render('homePage')
-})
-
 // Sign in page; if the email and password match the account in the database, redirect to songs library app
 router.post("/signIn", (req, res) => {
-    User.find({})
-        .then((userData) => res.render('songsLibPage', { userData }));
+    User.find({ email: req.body.email })
+        .then((userData) => {
+            // console.log(req.body)
+            // console.log(userData)
+
+            let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=500&q=${userTitle.title}&key=${process.env.API_KEY}`
+
+            if (userData[0].password === req.body.password) {
+
+                axios.get(url)
+                    .then((data) => {
+                        res.render('songsLibPage', { searchedTitle: data.data.items, userData: userData[0] });
+                    })
+            }
+            else {
+                res.redirect('signIn')
+            }
+
+        });
 });
 
-// Update a user by firstName in the database, then go to the home screen
+// Update the user in the database, then go to the home screen
 router.put("/:id", (req, res) => {
     User.findOneAndUpdate({ _id: req.params.id }, req.body).then(
-        (userData) => res.redirect('/')
+        (userData) => 
+        res.redirect('/')
     );
 });
 
-// Update a user by lastName in the database, then go to the home screen
-router.put("/:email", (req, res) => {
-    User.findOneAndUpdate({ name: req.params.email }, req.body).then(
-        (userData) => res.redirect('/')
-    );
-});
-
-// Update a user by lastName in the database, then go to the home screen
-router.put("/:password", (req, res) => {
-    User.findOneAndUpdate({ name: req.params.password }, req.body).then(
-        (userData) => res.redirect('/')
-    );
-});
-
-// Find a user in the database by id in the edit screen/window
+// Find the user in the database by id in the edit screen/window, shows that info. on edit page
 router.get('/:id/edit', (req, res) => {
-    User.findById(req.params.id).then(userData => {
-        res.render('editPage', { userData })
-    })
+    console.log(req.params.id)
+    User.findById(req.params.id)
+        .then(userData => {
+            console.log(req.params.id)
+            res.render('editPage', { userData })
+        })
+})
+
+// Find the user in the database by id in the admin screen/window, change the status to either "Actve" or "Inactive" to prevent the user from getting to the songLibPage. It will set the users email wrong, re-direct the user the homePage
+router.put('/:id/activation', (req, res) => {
+    User.findById(req.params.id)
+        .then(userData => {
+            res.render('editPage', { userData })
+        })
 })
 
 // Delete a user from database, then go to the home screen
@@ -77,46 +89,47 @@ router.delete("/:id", (req, res) => {
     );
 });
 
-router.get('/:id', (req, res) => {
-    res.render('homePage');
-})
-
-router.get('/homePage', (req, res) => {
-    res.redirect('homePage');
-})
-
-// Add a user to the database, then go to the home screen
-router.post("/signUp", (req, res) => {
-    User.create(req.body)
-        .then(() => res.redirect('/homePage'));
-});
-
 // The code below is for songs API and User interaction with the API
 // Sign in page; if the email and password match the account in the database, redirect to songs library app
-
-// The "/songsLib" is what I specified in the action in the forn in the ejs file
+// The "/songsLib" is what I specified in the action in the form in the ejs file
 // The "res.render('songsLibPage');" is where the information will be sent to.
-// This code is responsible of capturin searched information in the input box from songsLibPage
+// This code is responsible of capturing searched information in the input box from songsLibPage
 router.post("/songsLib", (req, res) => {
     userTitle = req.body
     // console.log(userTitle.title)
     //res.render('songsLibPage', { searchedTitle: userTitle.title });
 
-    let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${userTitle.title}&key=${process.env.API_KEY}`
+    // The url is loaded with the user's searched word, and ready to be used by Axios
+    let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=500&q=${userTitle.title}&key=${process.env.API_KEY}`
 
     axios.get(url)
-    .then((data) => {
-        // console.log(data.data.items[0].snippet.thumbnails.default.url)
-        res.render('songsLibPage', {searchedTitle: data.data.items});
-    })
-    
-    
+        .then((data) => {
+            console.log(data.data.items[0].snippet)
+            // console.log(data.data.items[0].snippet.thumbnails.default.url)
+            res.render('songsLibPage', { searchedTitle: data.data.items });
+        })
 });
 
-router.get('/songsLib', (req, res) => {
-        res.render('songsLibPage', { songs: userTitle.title });
+// The code below redirect the user to the update page when he/she clicks on forgot email or password
+router.put("/update/:id", (req, res) => {
+
+    User.findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then((userData) => res.redirect('/signIn')
+        );
+});
+
+// The code below renders the forget page to let the user update the email or password
+// When "forgot emai or password?" link is clicked, redirect to "forgotPage" page 
+router.get('/forgetPage', (req, res) => {
+    res.render('forgetPage');
 })
 
+// Update the user's email or password in the database, then go to the sign in page
+router.put("/update/:id", (req, res) => {
+    User.findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then((password) => res.redirect('signIn')
+        );
+});
 
 
 module.exports = router;
--- models/users-models.js
+++ models/users-models.js
@@ -3,32 +3,37 @@ const mongoose = require('../db/connection')
 // Structure data in database
 const UserSchema = new mongoose.Schema(
     {
-    firstName: {
-        type: String,
-        required: true
+        firstName: {
+            type: String,
+            required: true
+        },
+        middleName: {
+            type: String,
+            required: false
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        gender: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true,
+            default: true
+        }
     },
-    middleName: {
-        type: String,
-        required: false
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    status: String
-},
+
     {
 
         timestamps: String
